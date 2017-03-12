1451cec9a7da2b11f648f121ae601ebf2dae78a0
cowiefr <cowiefr@telus.net>
2017-03-12 14:16:02 -0600

    custom classes complete

---
+++ devslopes-social/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  devslopes-social
+//
+//  Created by Fain Cowie on 2017-03-12.
+//  Copyright © 2017 Fain Cowie. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
+let CrnrRad = 5.0
---
+++ devslopes-social/FancyBtn.swift
@@ -0,0 +1,23 @@
+//
+//  FancyBtn.swift
+//  devslopes-social
+//
+//  Created by Fain Cowie on 2017-03-12.
+//  Copyright © 2017 Fain Cowie. All rights reserved.
+//
+
+import UIKit
+
+class FancyBtn: UIButton {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
+        layer.shadowOpacity = 1
+        layer.shadowRadius = 5
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+        
+        layer.cornerRadius = CGFloat(CrnrRad)
+    }
+
+}
