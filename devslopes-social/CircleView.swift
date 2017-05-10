89eda314383ea9a0b592cb8c8ea80128c1166c03
cowiefr <cowiefr@telus.net>
2017-05-10 11:38:58 -0600

    Added elements to segue

--- devslopes-social/CircleView.swift
+++ devslopes-social/CircleView.swift
@@ -1,23 +1,21 @@
 //
-//  FancyView.swift
+//  CircleView.swift
 //  devslopes-social
 //
-//  Created by Fain Cowie on 2017-03-12.
+//  Created by Fain Cowie on 2017-05-10.
 //  Copyright © 2017 Fain Cowie. All rights reserved.
 //
 
 import UIKit
 
-class FancyView: UIView {
-
+class CircleView: UIImageView {
+    
     override func awakeFromNib() {
-        super.awakeFromNib()
-        
         layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
         layer.shadowOpacity = 1
         layer.shadowRadius = 5
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        layer.cornerRadius = 2.0
+        layer.cornerRadius = self.frame.width / 2
     }
 
 }
