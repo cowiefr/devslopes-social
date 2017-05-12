40c8e211b67480bf125cda622e559cb8e805c08e
cowiefr <cowiefr@telus.net>
2017-05-11 20:37:23 -0600

    FeedUI Complete

--- devslopes-social/CircleView.swift
+++ devslopes-social/CircleView.swift
@@ -10,12 +10,10 @@ import UIKit
 
 class CircleView: UIImageView {
     
-    override func awakeFromNib() {
-        layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
-        layer.shadowOpacity = 1
-        layer.shadowRadius = 5
-        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+    override func layoutSubviews() {
         layer.cornerRadius = self.frame.width / 2
+
     }
 
+    
 }
--- devslopes-social/FeedVC.swift
+++ devslopes-social/FeedVC.swift
@@ -10,13 +10,32 @@ import UIKit
 import Firebase
 import SwiftKeychainWrapper
 
-class FeedVC: UIViewController {
+class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        tableView.delegate = self
+        tableView.dataSource = self
+      }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+    }
+    
+    
     @IBAction func signOutTapped(_ sender: Any) {
-        KeychainWrapper.standard.remove(key: KEY_UID)
+        KeychainWrapper.standard.removeObject(forKey: KEY_UID)
         print("Fain: ID removed from Keychain")
         try! FIRAuth.auth()?.signOut()
         performSegue(withIdentifier: "goToSignIn", sender: nil)
